Fixed threshold calculation, progress on pruning

xgboost/RegressionTree.py
@@ -2,13 +2,14 @@ import numpy as np
 from xgboost.RegressionTreeNode import RegressionTreeNode
 
 class RegressionTree:
-    def __init__(self, data: np.ndarray, predictions: np.ndarray, learning_rate: float, gamma: float, _lambda: int=0, max_depth: int=6):
+    def __init__(self, x_data: np.ndarray, y_data: np.ndarray, predictions: np.ndarray, learning_rate: float, gamma: float, _lambda: int=0, max_depth: int=6):
         """
         Initializes the regression tree
 
         Args:
-            data          (np.ndarray): The data to train the tree on
-            predictions   (np.ndarray): Initial predictions
+            x_data        (np.ndarray): The x data
+            y_data        (np.ndarray): The y data
+            predictions   (np.ndarray): Initial predictions (of y values)
             learning_rate (float):      The learning rate
             gamma         (float):      Minimum gain for branch pruning
             _lambda       (int):        Regularization parameter (default to 0)
@@ -16,7 +17,8 @@ class RegressionTree:
         """
 
         # Load parameters
-        self.data = data
+        self.x_data = x_data
+        self.y_data = y_data
         self.predictions = predictions
         self.learning_rate = learning_rate
         self.gamma = gamma
@@ -34,14 +36,12 @@ class RegressionTree:
         Creates the root node of the tree
         """
 
-        # Calculate the residuals
-        residuals = self.predictions - self.data
-
-        return RegressionTreeNode(residuals, self._lambda)
+        return RegressionTreeNode(self.x_data, self.y_data, self.predictions, self._lambda)
 
     def grow_tree(self) -> None:
         """
-        Grows the tree by recursively creating children nodes until either the maximum depth is reached or there are no more residuals to split
+        Grows the tree by recursively creating children nodes until 
+        either the maximum depth is reached or there are no more residuals to split
         """
 
         nodes_to_check = [self.root]
@@ -63,3 +63,35 @@ class RegressionTree:
             else:
                 current_node_output = current_node.calculate_output(current_node.get_residuals())
                 current_node.set_output(current_node_output)
+
+    def prune_tree(self, node: RegressionTreeNode) -> None:
+        """
+        Recursively prunes a tree by checking if a node is a valid candidate for pruning and
+        checking if the gain is less than the gamma parameter. If so, the branch is pruned by
+        resetting it to a leaf node
+
+        Args:
+            node (RegressionTreeNode): The node to potentially prune the tree from
+        """
+
+        # Edge case checking for nodes that should NOT be pruned
+        left_child, right_child = node.get_left(), node.get_right()
+
+        # Edge case 1: The node is a leaf
+        if not left_child and not right_child:
+            return
+
+        # Edge case 2: One of the two children has children (protected ancestor)
+        if left_child and (left_child.get_left() or left_child.get_right()):
+            return
+        if right_child and (right_child.get_left() or right_child.get_right()):
+            return
+
+        # At this point, the node has two leaf children and is a valid candidate for pruning
+
+        # If the gain is less than the gamma parameter, prune the branch by condensing the node into a leaf node
+        if node.get_gain() < self.gamma:
+            node.condense()
+            
+
+        

xgboost/RegressionTreeNode.py
@@ -2,24 +2,29 @@ from __future__ import annotations
 import numpy as np
 
 class RegressionTreeNode:
-    def __init__(self, residuals: np.ndarray, _lambda: int, depth: int=1):
+    def __init__(self, x_data: np.ndarray, y_data: np.ndarray, predictions: np.ndarray, _lambda: int, depth: int=1):
         """
         Initializes the node along with the residuals, similarity, and gain
 
         Args:
-            residuals (np.ndarray): The residuals of the data
-            _lambda   (int):        Regularization parameter (default to 0)
-            depth     (int):        The depth of the node within the tree (default to 1)
+            x_data      (np.ndarray): The x data
+            y_data      (np.ndarray): The y data
+            predictions (np.ndarray): Initial predictions (of y values)
+            _lambda     (int):        Regularization parameter (default to 0)
+            depth       (int):        The depth of the node within the tree (default to 1)
         """
 
         # From the data
+        self.x_data = x_data
+        self.y_data = y_data
+        self.predictions = predictions
         self._lambda = _lambda
-        self.residuals = np.sort(residuals)
+        self.residuals = self.predictions - self.y_data
         self.depth = depth
 
         # Calculated values
         self.similarity = self.calculate_similarity(self.residuals)
-        self.gain = 0
+        self.gain = float('inf')
         self.threshold = 0
         self.output = float('inf')
 
@@ -87,11 +92,24 @@ class RegressionTreeNode:
 
         # Update the threshold and gain
         self.gain = best_gain
-        self.threshold = (self.residuals[split_index] + self.residuals[split_index - 1]) / 2
+        self.threshold = (self.x_data[split_index] + self.x_data[split_index - 1]) / 2
 
         # Create children using split index
-        self.left = RegressionTreeNode(self.residuals[:split_index], self._lambda, self.depth + 1)
-        self.right = RegressionTreeNode(self.residuals[split_index:], self._lambda, self.depth + 1)
+        self.left = RegressionTreeNode(self.x_data[:split_index], self.y_data[:split_index], self.predictions[:split_index], self._lambda, self.depth + 1)
+        self.right = RegressionTreeNode(self.x_data[split_index:], self.y_data[split_index:], self.predictions[split_index:], self._lambda, self.depth + 1)
+
+    def condense(self) -> None:
+        """
+        Condenses the node from being a branch to a leaf node
+        """
+
+        # Remove the children
+        self.left = None
+        self.right = None
+
+        # Update the output
+        self.output = self.calculate_output(self.residuals)
+        
 
     """
     GETTER METHODS
@@ -113,6 +131,12 @@ class RegressionTreeNode:
         Returns the depth of the node
         """
         return self.depth
+
+    def get_gain(self) -> float:
+        """
+        Returns the gain of the node
+        """
+        return self.gain
 
     def get_output(self) -> float:
         """
@@ -143,6 +167,18 @@ class RegressionTreeNode:
             output (float): The output to set
         """
         self.output = output
+
+    def set_left(self, left: RegressionTreeNode | None) -> None:
+        """
+        Sets the left child of the node
+        """
+        self.left = left
+
+    def set_right(self, right: RegressionTreeNode | None) -> None:
+        """
+        Sets the right child of the node
+        """
+        self.right = right
 
     """
     Overridden methods